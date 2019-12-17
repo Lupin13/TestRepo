f7ca75a05f5539bfd0b46babc914733d1c561eea
Enrico Capra <capraenrico13@gmail.com>
2019-12-17 10:34:52 +0100

    Added Scenes

---
+++ Assets/Scripts/Chicken.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chicken : MonoBehaviour
+{
+    public GameObject objectToSpawn;
+
+    private void Awake()
+    {
+        createNewEgg();
+    }
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void createNewEgg()
+    {
+        Instantiate(objectToSpawn, getSpawnPosition());
+    }
+
+    public Transform getSpawnPosition()
+    {
+        return this.gameObject.transform.parent;
+    }
+}
